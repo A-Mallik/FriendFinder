20e760cdaa9497257a980ccf46743aac948bfd5c
Shohan <ashraful.mallik100@gmail.com>
2019-01-17 14:20:45 -0500

    Added 7 more questions. 10 Total.

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var arrayofScores = [];
 
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// //for styling:
+// app.use(express.static(__dirname + '/public'));
 require('./app/routing/htmlRoutes.js')(app);
 require('./app/routing/apiRoutes.js')(app);
-//===========================================================// Create New Characters - takes in JSON input
 
 // //===========================================================
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
